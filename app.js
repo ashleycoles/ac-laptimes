66ea3904709b5148fef99844c60b41601985f58f
Ashley Coles <ashley@acoles.co.uk>
2020-06-10 14:29:40 +0100

    Adding cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./Config/routes');
+const cors = require('cors');
 const BodyParser = require('body-parser');
 const app = express()
 
 app.use(BodyParser.json());
+app.use(cors());
 
 routes(app);
 
